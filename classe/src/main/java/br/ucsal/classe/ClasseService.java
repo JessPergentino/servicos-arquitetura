f798d5d67b73e3853d638954322157e3b313f35a
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-11-10 16:34:09 -0300

    Projetos dos web services em rest (Buscar Classe e Buscar Ordem), mais a api do projeto

---
+++ classe/src/main/java/br/ucsal/classe/ClasseService.java
@@ -0,0 +1,41 @@
+package br.ucsal.classe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+
+import br.ucsal.heptofauna_api.Animal;
+import br.ucsal.heptofauna_api.Classe;
+
+@Path("/")
+public class ClasseService {
+
+	private static List<Animal> animais = new ArrayList<Animal>();
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{nomeCientifico}")
+	public Classe getContact(@PathParam("nomeCientifico") String nome) {
+
+		Classe result = null;
+
+		for (Animal animal : animais) {
+			if (animal.getNomeCientifico().equals(nome)) {
+				result = animal.getClasse();
+				break;
+			}
+		}
+
+		if (result == null) {
+			throw new WebApplicationException(404);
+		}
+
+		return result;
+	}
+}
